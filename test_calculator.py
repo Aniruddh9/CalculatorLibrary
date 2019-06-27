acaf9e1c3bc5e04ea81494a7d0c7f56436d4d21f
Aniruddh9 <admin@localhost.localdomain>
2019-06-27 11:55:52 +0000

    added multiplication

--- test_calculator.py
+++ test_calculator.py
@@ -2,12 +2,21 @@
     Testing the calculator functionalities.
 """
 
+
 import calculator
 
+
 class TestCalculator:
+
 
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
 
+
     def test_subtraction(self):
         assert 2 == calculator.subtract(5, 3)
+
+
+    def test_multiplication(self):
+        assert 10 == calculator.multiply(5, 2)
+
